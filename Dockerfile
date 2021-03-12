37a330171d1e4c1642028436b8bc88fff39bdd17
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-03-12 09:58:46 +0000

    sw-3418 added docker ignore

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
+FROM node:lts-alpine as asset-env
+
+WORKDIR /app
+
+RUN mkdir -p web/static
+
+COPY web/assets web/assets
+COPY build_assets.sh .
+COPY package.json .
+COPY yarn.lock .
+
+RUN yarn && sh build_assets.sh
+
 FROM golang:1.14 as build-env
 
 WORKDIR /app
@@ -13,10 +26,14 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o /go/
 
 FROM alpine:3.10
 
+WORKDIR /go/bin
+
 RUN apk --update --no-cache add \
     ca-certificates \
     && rm -rf /var/cache/apk/*
 RUN apk --no-cache add tzdata
 
 COPY --from=build-env /go/bin/opg-sirius-workflow /go/bin/opg-sirius-workflow
+COPY --from=build-env /app/web/template web/template
+COPY --from=asset-env /app/web/static web/static
 ENTRYPOINT ["/go/bin/opg-sirius-workflow"]
--- build_assest.sh
+++ build_assest.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 rm -rf web/static/*
 
-mkdir -p web/static/{javascript,stylesheets}
+mkdir -p web/static/stylesheets
 npx sass --load-path=. web/assets/main.scss web/static/stylesheets/all.css
-npx webpack --entry ./web/assets/main.js --output web/static/javascript/all.js
 
-mkdir -p web/static/assets/{fonts,images}
+mkdir -p web/static/javascript 
+npx webpack --mode production --entry ./web/assets/main.js --output web/static/javascript/all.js
+
+mkdir -p web/static/assets/fonts
 cp node_modules/govuk-frontend/govuk/assets/fonts/* web/static/assets/fonts
+
+mkdir -p web/static/assets/images
 cp node_modules/govuk-frontend/govuk/assets/images/* web/static/assets/images
 cp node_modules/@ministryofjustice/frontend/moj/assets/images/* web/static/assets/images
