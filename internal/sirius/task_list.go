25a54cdabc54aecdaab92cedd5454f417945e541
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-03-26 09:22:38 +0000

    SW-3420 cleaned up from last commit running as multi stacked array

--- internal/sirius/task_list.go
+++ internal/sirius/task_list.go
@@ -30,90 +30,26 @@ type caseItemsDetails struct {
 	CaseItemsUId  string        `json:"uId"`
 }
 
-// type AssigneeDetails struct {
-// 	DisplayName string `json:"displayName"`
-// 	AssigneeId  int    `json:"id"`
-// }
+type AssigneeDetails struct {
+	DisplayName string `json:"displayName"`
+	AssigneeId  int    `json:"id"`
+}
 
 type ApiTask struct {
-	Assignee struct {
-		DisplayName string `json:"displayName"`
-		Id          int    `json:"id"`
-	} `json:"assignee"`
-	// Assignee map[string]AssigneeDetails `json:"assignee"`
+	Assignee    AssigneeDetails    `json:"assignee"`
 	CaseItems   []caseItemsDetails `json:"caseItems"`
 	Clients     []string           `json:"clients"`
-	CreatedTime string             `json:"createdTime"`
 	Description string             `json:"description"`
 	DueDate     string             `json:"dueDate"`
 	ApiTaskId   int                `json:"id"`
 	Name        string             `json:"name"`
 	Persons     []string           `json:"persons"`
-	RagRating   int                `json:"ragRating"`
 	Status      string             `json:"status"`
-	Tasktype    string             `json:"type"`
 }
 
 type TaskList struct {
 	AllTaskList []ApiTask `json:"tasks"` //look into the type of this map for next time
 }
-
-// type MyTaskList struct {
-// 	Name                string
-// 	AssigneeDisplayName string
-// 	AssigneeId          int
-// }
-
-// "tasks":[{"id":123,"type":"CWGN","status":"Not started","dueDate":"01\/10\/2019","name":"Case work - General","description":"Case w   │
-// │   ork - General","ragRating":3,"assignee":{"id":65,"displayName":"case manager"},"createdTime":"25\/03\/2021 14:34:14","caseItems":[{"id":63,"uId":"7000-0000-2449","caseRecNumber":"05563462","client":{"   │
-// │   id":72,"uId":"7000-0000-2357","caseRecNumber":"84029229","salutation":"Duke","firstname":"John","middlenames":"","surname":"Fearless","supervisionCaseOwner":{"id":12,"displayName":"Allocations - (Supe   │
-// │   rvision)"}},"caseType":"ORDER","caseSubtype":"hw"}],"persons":[],"clients":[],"caseOwnerTask":false}]
-
-// "tasks":[
-//    {
-//       "id":123,
-//       "type":"CWGN",
-//       "status":"Not started",
-//       "dueDate":"01\/10\/2019",
-//       "name":"Case work - General",
-//       "description":"Case work - General",
-//       "ragRating":3,
-//       "assignee":{
-//          "id":65,
-//          "displayName":"case manager"
-//       },
-//       "createdTime":"25\/03\/2021 14:34:14",
-//       "caseItems":[
-//          {
-//             "id":63,
-//             "uId":"7000-0000-2449",
-//             "caseRecNumber":"05563462",
-//             "client":{
-//                "id":72,
-//                "uId":"7000-0000-2357",
-//                "caseRecNumber":"84029229",
-//                "salutation":"Duke",
-//                "firstname":"John",
-//                "middlenames":"",
-//                "surname":"Fearless",
-//                "supervisionCaseOwner":{
-//                   "id":12,
-//                   "displayName":"Allocations - (Supervision)"
-//                }
-//             },
-//             "caseType":"ORDER",
-//             "caseSubtype":"hw"
-//          }
-//       ],
-//       "persons":[
-
-//       ],
-//       "clients":[
-
-//       ],
-//       "caseOwnerTask":false
-//    }
-// ]
 
 func (c *Client) GetTaskList(ctx Context) ([]ApiTask, error) {
 	req, err := c.newRequest(ctx, http.MethodGet, "/api/v1/assignees/65/tasks", nil)
@@ -147,39 +83,25 @@ func (c *Client) GetTaskList(ctx Context) ([]ApiTask, error) {
 
 	// allTaskList := make([]MyTaskList, len(v))
 
-	var taskList []ApiTask
+	// var taskList []ApiTask
 
-	for _, u := range allTaskList {
-		task := ApiTask{
-			// Assignee:    u.Assignee,
-			// CaseItems:   u.CaseItems,
-			Clients:     u.Clients,
-			CreatedTime: u.CreatedTime,
-			Description: u.Description,
-			DueDate:     u.DueDate,
-			ApiTaskId:   u.ApiTaskId,
-			Name:        u.Name,
-			Persons:     u.Persons,
-			RagRating:   u.RagRating,
-			Status:      u.Status,
-			Tasktype:    u.Tasktype,
-		}
-		task.Assignee.DisplayName = u.Assignee.DisplayName
-		task.Assignee.Id = u.Assignee.Id
-
-		taskList = append(taskList, task)
-	}
-
-	// for i, t := range v {
-	// 	allTaskList[i] = MyTaskList{
-	// 		Name: t.Name,
+	// for _, u := range allTaskList {
+	// 	task := ApiTask{
+	// 		// Assignee:    u.Assignee,
+	// 		// CaseItems:   u.CaseItems,
+	// 		// Clients:     u.Clients,
+	// 		// CreatedTime: u.CreatedTime,
+	// 		// Description: u.Description,
+	// 		// DueDate:     u.DueDate,
+	// 		// ApiTaskId:   u.ApiTaskId,
+	// 		// Name:        u.Name,
+	// 		// Persons:     u.Persons,
+	// 		// RagRating:   u.RagRating,
+	// 		// Status:      u.Status,
+	// 		//Tasktype: u.Tasktype,
 	// 	}
 
-	// if t.Assignee != nil {
-	// 	allTaskList[i].AssigneeDisplayName = t.Assignee.DisplayName
-	// 	allTaskList[i].AssigneeId = t.Assignee.Id
+	// 	taskList = append(taskList, task)
 	// }
-	// }
-
 	return allTaskList, err
 }
