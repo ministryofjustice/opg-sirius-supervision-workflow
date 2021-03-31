6c90b27890896b25e9df464be9f4f25e1cd3538e
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-03-31 14:31:45 +0100

    added in config for puppeteer, pally and lighthouse

---
+++ docker/puppeteer/lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://app:8888/sirius/workflow'],
+      settings: {
+        extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
+        chromeFlags: "--disable-gpu --no-sandbox",
+      },
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", {"minScore": 0.9}],
+        "categories:accessibility": ["error", {"minScore": 1}],
+        "categories:best-practices": ["warn", {"minScore": 0.9}],
+        "categories:seo": ["warn", {"minScore": 0.9}],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
