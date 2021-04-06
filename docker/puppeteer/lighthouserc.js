be6c0c9e0b6305197a651322c12e4520762967e5
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 10:09:41 +0100

    amended url to correct path

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://app:8888/sirius/workflow'],
+      url: ['http://app:8888/supervision/workflow'],
       settings: {
         extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
         chromeFlags: "--disable-gpu --no-sandbox",
