ab24eb73f8ef4e9339ae10543e8228c9cdc37f77
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 11:38:09 +0100

    amended changes back to pa11y form as failing to run

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -12,7 +12,7 @@ module.exports = {
         "categories:performance": ["warn", {"minScore": 0.9}],
         "categories:accessibility": ["error", {"minScore": 1}],
         "categories:best-practices": ["warn", {"minScore": 0.9}],
-        "categories:seo": ["warn", {"minScore": 0.5}],
+        "categories:seo": ["warn", {"minScore": 0.9}],
       },
     },
     upload: {
