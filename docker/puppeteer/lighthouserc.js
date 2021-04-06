fd0dc6e9897b094a12f66832eac46f2dd7e22178
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 10:22:43 +0100

    moving lighthouse url to https

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://app:8888/supervision/workflow'],
+      url: ['https://app:8888/supervision/workflow'],
       settings: {
         extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
         chromeFlags: "--disable-gpu --no-sandbox",
@@ -10,7 +10,7 @@ module.exports = {
     assert: {
       assertions: {
         "categories:performance": ["warn", {"minScore": 0.9}],
-        "categories:accessibility": ["error", {"minScore": 1}],
+        "categories:accessibility": ["error", {"minScore": 1}],ß
         "categories:best-practices": ["warn", {"minScore": 0.9}],
         "categories:seo": ["warn", {"minScore": 0.9}],
       },
