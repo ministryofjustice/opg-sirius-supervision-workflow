176f79e8bd5f82b8f18d49eb7003efff76b42ac1
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 11:48:50 +0100

    remove s from url

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://app:8888/supervision/workflow'],
+      url: ['http://app:8888/supervision/workflow'],
       settings: {
         extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
         chromeFlags: "--disable-gpu --no-sandbox",
