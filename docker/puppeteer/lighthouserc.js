a9309e4396a5892093a99e943b4951b8acb52aa9
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 10:57:20 +0100

    fix linting error

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -10,7 +10,7 @@ module.exports = {
     assert: {
       assertions: {
         "categories:performance": ["warn", {"minScore": 0.9}],
-        "categories:accessibility": ["error", {"minScore": 1}],ß
+        "categories:accessibility": ["error", {"minScore": 1}],
         "categories:best-practices": ["warn", {"minScore": 0.9}],
         "categories:seo": ["warn", {"minScore": 0.5}],
       },
