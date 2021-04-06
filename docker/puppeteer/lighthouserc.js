9dcebf6a12453dc9082436c33a93ff8b8ad14e6a
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-06 10:43:18 +0100

    trying to remove seo tests from lighthouse file

--- docker/puppeteer/lighthouserc.js
+++ docker/puppeteer/lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://app:8888/supervision/workflow'],
+      url: ['http://app:8888/supervision/workflow'],
       settings: {
         extraHeaders: JSON.stringify({Cookie: 'XSRF-TOKEN=abcde; Other=other'}),
         chromeFlags: "--disable-gpu --no-sandbox",
@@ -12,7 +12,6 @@ module.exports = {
         "categories:performance": ["warn", {"minScore": 0.9}],
         "categories:accessibility": ["error", {"minScore": 1}],ß
         "categories:best-practices": ["warn", {"minScore": 0.9}],
-        "categories:seo": ["warn", {"minScore": 0.9}],
       },
     },
     upload: {
