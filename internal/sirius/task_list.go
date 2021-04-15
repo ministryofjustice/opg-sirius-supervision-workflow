d615c103edcbf088b2942830c5c5f634cd0b32e3
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-15 10:59:51 +0100

    loads teams tasks

--- internal/sirius/task_list.go
+++ internal/sirius/task_list.go
@@ -118,11 +118,11 @@ func (c *Client) GetTaskList(ctx Context, search int, displayTaskLimit int, sele
 	var v TaskList
 	var k TaskDetails
 	log.Println(selectedTeamMembers)
-	teamID := selectedTeamMembers.Id
+	teamID := selectedTeamMembers.Members[0].TeamMembersId
 	log.Println(teamID)
 
-	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/assignees/team/%d/tasks?limit=%d&page=%d&sort=dueDate:asc", teamID, displayTaskLimit, search), nil)
-	// req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/assignees/team/tasks/%d?limit=%d&page=%d&sort=dueDate:asc", teamID, displayTaskLimit, search), nil)
+	// req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/assignees/team/%d/tasks?limit=%d&page=%d&sort=dueDate:asc", teamID, displayTaskLimit, search), nil)
+	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/assignees/team/tasks/%d?limit=%d&page=%d&sort=dueDate:asc", teamID, displayTaskLimit, search), nil)
 	if err != nil {
 		return v, k, err
 	}
