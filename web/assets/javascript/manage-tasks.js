12cf6c59518e6532bec89bb3cb459871d20f8811
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-04-23 09:15:46 +0100

    should have manage tasks button and edit panel toggling

--- web/assets/javascript/manage-tasks.js
+++ web/assets/javascript/manage-tasks.js
@@ -6,11 +6,16 @@ export default class ManageTasks {
         this.checkBoxElements = element.querySelectorAll('[data-wf-module="manage-tasks_checkbox"]');
         this.selectedCountElement = element.querySelectorAll('[data-wf-module="manage-tasks_task-count"]')[0];
         this.allcheckBoxElements = element.querySelectorAll('[data-wf-module="manage-tasks_all-checkboxes"]')[0];
+        
         this._bindAllCheckBox(this.allcheckBoxElements);
 
         this.checkBoxElements.forEach(checkbox => {
             this._bindCheckBox(checkbox);
         });
+
+        this.manageTasksButton = element.querySelectorAll('[data-wf-module="manage-tasks_edit-task-btn"]')[0];
+        this.editPanelDiv = element.querySelectorAll('[data-wf-module="manage-tasks_edit-panel"]')[0];
+        this._bindAllCheckBox(this.manageTasksButton);
     }
 
     numberOfTasksSelected() {
@@ -19,6 +24,7 @@ export default class ManageTasks {
 
     _updateDomElements() {
         this.selectedCountElement.innerText = this.numberOfTasksSelected().toString();
+        this._showManageTasksButton();
     }
 
     _bindCheckBox(element) {
@@ -28,7 +34,7 @@ export default class ManageTasks {
 
     _updateSelectedState(event) {
         event.target.checked ? this.data.selectedTasks++ : this.data.selectedTasks--;
-        this._updateDomElements();
+        this._updateDomElements();   
     }
 
     _bindAllCheckBox(element) {
@@ -44,7 +50,19 @@ export default class ManageTasks {
         });
 
         this.data.selectedTasks = (isChecked ? this.checkBoxElements.length : 0);
-
         this._updateDomElements();
     }
+
+    _showManageTasksButton() {
+      this.manageTasksButton.classList.toggle('hide', this.data.selectedTasks === 0);
+    }
+
+    _bindShowManageTasksButton(element) {
+      this._showEditTasksPanel = this._showEditTasksPanel.bind(this);
+      element.addEventListener('click', this._showEditTasksPanel);
+    }
+
+   _showEditTasksPanel(event) {
+    this.editPanelDiv.classList.toggle('hide', this.data.selectedTasks === 0);
+   }
 }
