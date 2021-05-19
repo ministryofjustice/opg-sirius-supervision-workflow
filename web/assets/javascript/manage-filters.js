205f80e0416c5ff5438900d5af15be6b9ff9e716
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-05-19 12:00:46 +0100

    sw-3423 added in js for the drop down of task types

--- web/assets/javascript/manage-filters.js
+++ web/assets/javascript/manage-filters.js
@@ -20,9 +20,13 @@ export default class ManageFilters {
         const hiddenState = this.innerContainer.classList.contains('hide');
         this.innerContainer.classList.toggle("hide", !hiddenState)
         if (hiddenState) {
+            this.taskTypeFilterArrowUp.setAttribute('aria-expanded', 'true')
+            this.taskTypeFilterArrowDown.setAttribute('aria-expanded', 'false')
             this.taskTypeFilterArrowUp.classList.toggle("hide", true)
             this.taskTypeFilterArrowDown.classList.toggle("hide", false)
         } else {
+            this.taskTypeFilterArrowUp.setAttribute('aria-expanded', 'false')
+            this.taskTypeFilterArrowDown.setAttribute('aria-expanded', 'true')
             this.taskTypeFilterArrowUp.classList.toggle("hide", false)
             this.taskTypeFilterArrowDown.classList.toggle("hide", true)
         }
