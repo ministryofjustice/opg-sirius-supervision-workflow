a5a6b98f85ee15cbaa69841814314c09f8353630
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-05-25 10:12:54 +0100

    sw-3423 remove task type filters from task list

--- internal/sirius/task_list_test.go
+++ internal/sirius/task_list_test.go
@@ -115,7 +115,7 @@ func TestTaskList(t *testing.T) {
 			tc.setup()
 			assert.Nil(t, pact.Verify(func() error {
 				client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
-				taskList, taskDetails, err := client.GetTaskList(getContext(tc.cookies), 1, 25, 13, 13, []string{"CWGN", "CNC"})
+				taskList, taskDetails, err := client.GetTaskList(getContext(tc.cookies), 1, 25, 13, 13, []string{})
 				assert.Equal(t, tc.expectedResponse.WholeTaskList[0], taskList.WholeTaskList[0], taskDetails)
 				assert.Equal(t, tc.expectedError, err)
 				return nil
