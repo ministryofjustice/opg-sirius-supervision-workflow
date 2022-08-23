f5dfc7900819106aa250681ce9545334eb4b84ae
Kate <kate.smith@digital.justice.gov.uk>
2022-08-23 09:18:44 +0100

    sw-5907 removed fetch fix

--- cypress/e2e/workflow.cy.js
+++ cypress/e2e/workflow.cy.js
@@ -24,14 +24,14 @@ describe("Work flow", () => {
   })
 
   it("the nav link should contain supervision", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("have.attr", "href", "http://localhost:3000/supervision")
+    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("have.attr", "href", "http://localhost:8080/supervision")
   })  
   
   it("the nav link should contain lpa", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(2) > a").should("have.attr", "href", "http://localhost:3000/lpa")
+    cy.get(".moj-header__navigation-list > :nth-child(2) > a").should("have.attr", "href", "http://localhost:8080/lpa")
   })
   
   it("the nav link should contain logout", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(3) > a").should("have.attr", "href", "http://localhost:3000/auth/logout")
+    cy.get(".moj-header__navigation-list > :nth-child(3) > a").should("have.attr", "href", "http://localhost:8080/auth/logout")
   }) 
 });
