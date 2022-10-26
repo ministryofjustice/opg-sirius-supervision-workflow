8b8cfb279bc0b80e701980fffb79cb4619a81702
Kate <kate.smith@digital.justice.gov.uk>
2022-10-26 10:37:16 +0100

    sw-5910 improved existing cypress tests

--- cypress/e2e/pagination.cy.js
+++ cypress/e2e/pagination.cy.js
@@ -5,29 +5,36 @@ describe("Pagination", () => {
       cy.visit("/supervision/workflow/1");
   });
 
-  it("shows correct number of total tasks", () => {
-    cy.get("#pagination-label > .flex-container > .moj-pagination__results > :nth-child(1)").should('contain', '1')
-    cy.get("#pagination-label > .flex-container > .moj-pagination__results > :nth-child(2)").should('contain', '10')
-    cy.get("#pagination-label > .flex-container > .moj-pagination__results > :nth-child(3)").should('contain', '10')
-  })
+  // it("does not show previous button on page one", () => {
+  //   cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('not.be.visible', 'Previous')
+  // })
 
- it("disabled previous button while on top page one", () => {
-    cy.get("#pagination-label > .flex-container > .moj-pagination__list > .moj-pagination__item--prev > .moj-pagination__link").should('be.disabled')
+  it("shows next button on page one", () => {
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', 'Next')
   })
 
   it("can select 25 from task view value dropdown", () => {
     cy.get("#display-rows").select('25')
     cy.get("#display-rows").should('have.value', '25')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '1')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '25')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '101')
   })
 
   it("can select 50 from task view value dropdown", () => {
     cy.get("#display-rows").select('50')
     cy.get("#display-rows").should('have.value', '50')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '1')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '50')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '101')
   })
 
   it("can select 100 from task view value dropdown", () => {
     cy.get("#display-rows").select('100')
     cy.get("#display-rows").should('have.value', '100')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '1')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '100')
+    cy.get(".govuk-\\!-padding-right-2 > #pagination > .flex-container").should('contain', '101')
   })
 
 });
