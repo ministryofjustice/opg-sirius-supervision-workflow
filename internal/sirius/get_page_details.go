44cc92b1ca921d79ecef46fa154eec359a3babff
Kate Smith <52196833+kate-49@users.noreply.github.com>
2022-11-08 12:41:03 +0000

    Sw 5910 pagination tests (#242)
    * Revert "Sw 5907 adding cypress and unit testing (#207)"
    
    This reverts commit 0a91af1b6a31a7eea62cf7f9626509647e0a0ea7.
    
    * sw-5910 move pagiation to new component but amend for buttons
    
    * sw-5910 pagination numbers show as expected but move around
    
    * sw-5910 improved existing cypress tests
    
    * sw-5910 set up different routes for page limits
    
    * sw-5910 working solution for changing pages but bit messy
    
    * sw-5910 tests cover pagination reacts as expected but have to hide rather than use visibility hidden
    
    * sw-5910 cover other test cases fix linting
    
    * sw-5910 add new element to unit tests
    
    * sw-5910 all cypress tests working locally
    
    * sw-5910 remove unused css
    
    * sw-5910 uncomment test
    
    * sw-5910 fix css styling
    
    * Fix current item CSS and move to template if not visibility hidden
    
    * Fix test assertions for not-visible.
    
    * sw-5910 clean up cypress test selectors
    
    * sw-5910 fix db generating data for id 0
    
    Co-authored-by: Matt Machell <matt.machell@digital.justice.gov.uk>

--- internal/sirius/get_page_details.go
+++ internal/sirius/get_page_details.go
@@ -1,16 +1,17 @@
 package sirius
 
 type PageDetails struct {
-	ListOfPages       []int
-	PreviousPage      int
-	NextPage          int
-	LimitedPagination []int
-	FirstPage         int
-	LastPage          int
-	StoredTaskLimit   int
-	ShowingUpperLimit int
-	ShowingLowerLimit int
-	LastFilter        string
+	ListOfPages        []int
+	PreviousPage       int
+	NextPage           int
+	LimitedPagination  []int
+	FirstPage          int
+	LastPage           int
+	StoredTaskLimit    int
+	ShowingUpperLimit  int
+	ShowingLowerLimit  int
+	LastFilter         string
+	UpperEllipsesLimit bool
 }
 
 func (c *Client) GetPageDetails(tasklist TaskList, search int, displayTaskLimit int) PageDetails {
@@ -31,6 +32,8 @@ func (c *Client) GetPageDetails(tasklist TaskList, search int, displayTaskLimit 
 	PageDetails.ShowingUpperLimit = GetShowingUpperLimitNumber(tasklist, displayTaskLimit)
 
 	PageDetails.ShowingLowerLimit = GetShowingLowerLimitNumber(tasklist, displayTaskLimit)
+
+	PageDetails.UpperEllipsesLimit = GetUpperEllipsesLimit(tasklist.Pages, search)
 
 	if len(PageDetails.ListOfPages) != 0 {
 		PageDetails.FirstPage = PageDetails.ListOfPages[0]
@@ -100,4 +103,8 @@ func GetPaginationLimits(taskList TaskList, PageDetails PageDetails) []int {
 		twoAfterCurrentPage = taskList.Pages.PageCurrent
 	}
 	return PageDetails.ListOfPages[twoBeforeCurrentPage:twoAfterCurrentPage]
+}
+
+func GetUpperEllipsesLimit(pages PageInformation, search int) bool {
+	return (pages.PageTotal - search) >= 3
 }
