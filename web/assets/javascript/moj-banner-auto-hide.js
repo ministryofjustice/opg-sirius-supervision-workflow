20dc88fc34b67e1e0de7797d1b1dec5db03668a8
Matt Machell <matt.machell@digital.justice.gov.uk>
2022-11-09 16:34:45 +0000

    Run prettier on the web folder

--- web/assets/javascript/moj-banner-auto-hide.js
+++ web/assets/javascript/moj-banner-auto-hide.js
@@ -1,13 +1,12 @@
 const MojBannerAutoHide = (element) => {
-  
   const elements = element.querySelectorAll(
     '[data-module="moj-banner-auto-hide"]'
   );
 
   elements.forEach((bannerElement) => {
-    setTimeout(function() {
-      bannerElement.classList.add('hide');
-    }, 5000)
+    setTimeout(function () {
+      bannerElement.classList.add("hide");
+    }, 5000);
   });
 };
 
