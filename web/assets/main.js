75da8f6e9d75ce51481934ad33ec76f76b6feae9
devkokov <dm.kokov@gmail.com>
2022-11-18 14:37:54 +0000

    SW-5854 Replace Webpack with ESBuild

--- web/assets/main.js
+++ web/assets/main.js
@@ -1,4 +1,3 @@
-import "./main.scss";
 import GOVUKFrontend from "govuk-frontend/govuk/all.js";
 import MOJFrontend from "@ministryofjustice/frontend/moj/all.js";
 import ManageTasks from "./javascript/manage-tasks";
