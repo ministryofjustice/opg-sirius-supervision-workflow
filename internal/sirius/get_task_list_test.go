95d26d05c0ac16af4e307adea258ca1bf1b21c48
devkokov <dm.kokov@gmail.com>
2023-04-13 09:57:24 +0100

    SW-6429 Display deputy name on the task list for PA teams

--- internal/sirius/get_task_list_test.go
+++ internal/sirius/get_task_list_test.go
@@ -472,3 +472,33 @@ func setUpPagesTests(pageCurrent int, lastPage int) (TaskList, PageDetails) {
 
 	return taskList, pageDetails
 }
+
+func TestDeputy_GetURL(t *testing.T) {
+	tests := []struct {
+		name        string
+		deputyType  string
+		expectedUrl string
+	}{
+		{
+			name:        "Professional deputy URL",
+			deputyType:  "PRO",
+			expectedUrl: "/supervision/deputies/13",
+		},
+		{
+			name:        "PA deputy URL",
+			deputyType:  "PA",
+			expectedUrl: "/supervision/deputies/13",
+		},
+		{
+			name:        "Lay deputy URL",
+			deputyType:  "LAY",
+			expectedUrl: "/supervision/#/deputy-hub/13",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			deputy := Deputy{Id: 13, Type: RefData{Handle: test.deputyType}}
+			assert.Equal(t, test.expectedUrl, deputy.GetURL())
+		})
+	}
+}
