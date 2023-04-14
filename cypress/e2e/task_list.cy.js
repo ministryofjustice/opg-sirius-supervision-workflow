9dd95b743b4fdcdb68ca4133e0f8d40e9dccfe24
devkokov <dm.kokov@gmail.com>
2023-04-14 10:45:18 +0100

    SW-6429 Fix syntax error

--- cypress/e2e/task_list.cy.js
+++ cypress/e2e/task_list.cy.js
@@ -43,6 +43,7 @@ describe("Task list", () => {
         .then(href => {
           expect(href).to.contains("/supervision/#/clients/3333");
         })
+  })
 
   it("should display deputy name for PA teams", () => {
     cy.get('.moj-team-banner__container > .govuk-form-group > .govuk-select').select('PA Team 1 - (Supervision)')
