89a326e1b4d73e98ca51766e8b1a9463a1755c32
samuel.joseph <denialanderror@googlemail.com>
2023-05-15 18:04:46 +0100

    sw-5960: Adds ECM Tasks filter

--- internal/sirius/get_task_types_test.go
+++ internal/sirius/get_task_types_test.go
@@ -32,6 +32,11 @@ func TestGetTaskTypes(t *testing.T) {
 
 	expectedResponse := []ApiTaskTypes{
 		{
+			Handle:     "ECM_TASKS",
+			Incomplete: "ECM Tasks",
+			IsSelected: false,
+		},
+		{
 			Handle:     "CWGN",
 			Incomplete: "Casework - General",
 			Category:   "supervision",
@@ -79,6 +84,11 @@ func TestGetTaskTypesCanMarkSelected(t *testing.T) {
 
 	expectedResponse := []ApiTaskTypes{
 		{
+			Handle:     "ECM_TASKS",
+			Incomplete: "ECM Tasks",
+			IsSelected: false,
+		},
+		{
 			Handle:     "CWGN",
 			Incomplete: "Casework - General",
 			Category:   "supervision",
@@ -105,6 +115,64 @@ func TestGetTaskTypesCanMarkSelected(t *testing.T) {
 	}
 
 	taskTypes, err := client.GetTaskTypes(getContext(nil), []string{"CWGN", "CNC"})
+	assert.Equal(t, expectedResponse, taskTypes)
+	assert.Equal(t, nil, err)
+}
+
+func TestGetTaskTypesCanMarkSelectedForEcmTasks(t *testing.T) {
+	logger, mockClient := SetUpTest()
+	client, _ := NewClient(mockClient, "http://localhost:3000", logger)
+
+	json := `{
+		"task_types":{
+			"CWGN":{"handle":"CWGN","incomplete":"Casework - General","complete":"Casework - General","user":true,"category":"supervision"},
+			"CNC":{"handle":"CNC","incomplete":"Casework - Non-compliant","complete":"Casework - compliant","user":true,"category":"supervision"},
+			"FCIC":{"handle": "FCIC","incomplete": "First Contact - Introductory call","complete": "First Contact - Introductory called","user": true,"category": "supervision"}
+		}
+    }`
+
+	r := io.NopCloser(bytes.NewReader([]byte(json)))
+
+	mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	expectedResponse := []ApiTaskTypes{
+		{
+			Handle:     "ECM_TASKS",
+			Incomplete: "ECM Tasks",
+			IsSelected: true,
+		},
+		{
+			Handle:     "CWGN",
+			Incomplete: "Casework - General",
+			Category:   "supervision",
+			Complete:   "Casework - General",
+			User:       true,
+			IsSelected: false,
+		},
+		{
+			Handle:     "CNC",
+			Incomplete: "Casework - Non-compliant",
+			Category:   "supervision",
+			Complete:   "Casework - compliant",
+			User:       true,
+			IsSelected: false,
+		},
+		{
+			Handle:     "FCIC",
+			Incomplete: "First Contact - Introductory call",
+			Category:   "supervision",
+			Complete:   "First Contact - Introductory called",
+			User:       true,
+			IsSelected: false,
+		},
+	}
+
+	taskTypes, err := client.GetTaskTypes(getContext(nil), []string{"ECM_TASKS"})
 	assert.Equal(t, expectedResponse, taskTypes)
 	assert.Equal(t, nil, err)
 }
