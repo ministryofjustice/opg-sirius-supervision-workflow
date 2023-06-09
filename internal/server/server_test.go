c493040b246c3bd15282c5900f125e9f8ad64c90
devkokov <dm.kokov@gmail.com>
2023-06-09 14:37:23 +0100

    SW-6059 Fix unit tests

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -6,13 +6,11 @@ import (
 
 type mockTemplates struct {
 	count    int
-	lastName string
 	lastVars interface{}
 }
 
-func (m *mockTemplates) ExecuteTemplate(w io.Writer, name string, vars interface{}) error {
+func (m *mockTemplates) Execute(w io.Writer, vars interface{}) error {
 	m.count += 1
-	m.lastName = name
 	m.lastVars = vars
 	return nil
 }
