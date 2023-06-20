018f05ee9702be015828bcec25c40b8474b9e313
Kate <kate.smith@digital.justice.gov.uk>
2023-06-20 13:33:00 +0100

    sw-5332 amend workflow link in tests

--- cypress/e2e/workflow.cy.js
+++ cypress/e2e/workflow.cy.js
@@ -32,18 +32,18 @@ describe("Workflow", () => {
   })
 
   it("the nav link should contain lpa", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("have.attr", "href", "http://localhost:8080/lpa")
+    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("contain.value", "href", "/lpa")
   })
 
   it("the nav link should contain supervision", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("have.attr", "href", "http://localhost:8080/supervision")
+    cy.get(".moj-header__navigation-list > :nth-child(1) > a").should("contain.value", "href", "/supervision")
   })  
   
   it("the nav link should contain lpa", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(2) > a").should("have.attr", "href", "http://localhost:8080/admin")
+    cy.get(".moj-header__navigation-list > :nth-child(2) > a").should("contain.value", "href", "/admin")
   })
   
   it("the nav link should contain logout", () => {
-    cy.get(".moj-header__navigation-list > :nth-child(3) > a").should("have.attr", "href", "http://localhost:8080/auth/logout")
+    cy.get(".moj-header__navigation-list > :nth-child(3) > a").should("contain.value", "href", "/auth/logout")
   })
 });
