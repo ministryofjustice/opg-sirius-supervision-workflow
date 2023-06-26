dd113636fd26bc3e928abbab2108f34ccde6feb6
devkokov <dm.kokov@gmail.com>
2023-06-26 17:55:31 +0100

    More cleanup after rebase

--- internal/sirius/get_task_list.go
+++ internal/sirius/get_task_list.go
@@ -43,6 +43,8 @@ type Client struct {
 	Surname              string   `json:"surname"`
 	SupervisionCaseOwner Assignee `json:"supervisionCaseOwner"`
 	FeePayer             Deputy   `json:"feePayer"`
+	Orders               []Order  `json:"cases"`
+	SupervisionLevel     string   `json:"supervisionLevel"`
 }
 
 type Task struct {
@@ -220,6 +222,31 @@ func (t Task) GetAssignee() Assignee {
 		}
 	}
 	return t.Assignee
+}
+
+func (c Client) GetReportDueDate() string {
+	if len(c.Orders) > 0 {
+		return c.Orders[0].LatestAnnualReport.DueDate
+	}
+	return ""
+}
+
+func (c Client) GetStatus() string {
+	orderStatuses := make(map[string]string)
+
+	for _, order := range c.Orders {
+		label := order.Status.Label
+		orderStatuses[label] = label
+	}
+
+	statuses := []string{"Active", "Open", "Closed", "Duplicate"}
+	for _, status := range statuses {
+		if _, found := orderStatuses[status]; found {
+			return status
+		}
+	}
+
+	return ""
 }
 
 func getEcmTaskTypesString(taskTypes []TaskType) []string {
--- internal/sirius/get_task_list_test.go
+++ internal/sirius/get_task_list_test.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -478,4 +479,50 @@ func TestTask_GetDueDateStatus(t *testing.T) {
 			assert.Equal(t, test.want, task.GetDueDateStatus(mockNow))
 		})
 	}
+}
+
+func TestClient_GetStatus(t *testing.T) {
+	tests := []struct {
+		orderStatuses []string
+		wantStatus    string
+	}{
+		{
+			orderStatuses: []string{"Closed", "Open", "Duplicate", "Active", "Closed", "Open", "Duplicate"},
+			wantStatus:    "Active",
+		},
+		{
+			orderStatuses: []string{"Open", "Duplicate", "Closed", "Open", "Duplicate"},
+			wantStatus:    "Open",
+		},
+		{
+			orderStatuses: []string{"Duplicate", "Closed", "Duplicate"},
+			wantStatus:    "Closed",
+		},
+		{
+			orderStatuses: []string{"Duplicate"},
+			wantStatus:    "Duplicate",
+		},
+	}
+	for i, test := range tests {
+		t.Run("Scenario "+strconv.Itoa(i+1), func(t *testing.T) {
+			var client Client
+			for _, status := range test.orderStatuses {
+				client.Orders = append(client.Orders, Order{Status: RefData{Label: status}})
+			}
+			assert.Equal(t, test.wantStatus, client.GetStatus())
+		})
+	}
+}
+
+func TestClient_GetReportDueDate(t *testing.T) {
+	client := Client{
+		Orders: []Order{
+			{
+				LatestAnnualReport: AnnualReport{
+					DueDate: "12/02/2020",
+				},
+			},
+		},
+	}
+	assert.Equal(t, "12/02/2020", client.GetReportDueDate())
 }
