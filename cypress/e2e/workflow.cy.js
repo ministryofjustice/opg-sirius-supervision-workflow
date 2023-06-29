1014fa92daf73161820d56632d7ed3103e47eff0
Kate Smith <52196833+kate-49@users.noreply.github.com>
2023-06-29 17:32:06 +0100

    sw-5332 fix cypress

--- cypress/e2e/workflow.cy.js
+++ cypress/e2e/workflow.cy.js
@@ -13,7 +13,7 @@ describe("Workflow", () => {
   });
 
   it("should load header template within banner", () => {
-    cy.get('.moj-header__logo > .moj-header__link').should('contain.text', 'OPG');
-    cy.contains(".moj-header__link", "Sirius");
+      cy.get('.govuk-header__link--homepage').should('contain.text', 'OPG');
+      cy.get('.govuk-header__service-name').should('contain.text', 'Sirius')
   });
 });
