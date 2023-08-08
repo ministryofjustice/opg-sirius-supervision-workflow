3d7e5d855d5077631459f62d26aa28e21961609d
samuel.joseph <denialanderror@googlemail.com>
2023-08-08 10:47:32 +0100

    sw-6708: bump header

--- internal/server/workflow_vars.go
+++ internal/server/workflow_vars.go
@@ -17,6 +17,7 @@ type WorkflowVars struct {
 	SelectedTeam    model.Team
 	Tabs            []Tab
 	SuccessMessage  string
+	Error           string
 	Errors          sirius.ValidationErrors
 	EnvironmentVars EnvironmentVars
 }
