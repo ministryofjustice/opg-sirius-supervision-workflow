171b566c59a9a023e18c37b060c22422071faa43
devkokov <dm.kokov@gmail.com>
2023-08-22 17:02:56 +0100

    SW-6740 Error handling for both types of status errors (Sirius and Server)

--- internal/server/wrap_handler_test.go
+++ internal/server/wrap_handler_test.go
@@ -8,6 +8,7 @@ import (
 	"go.uber.org/zap/zaptest/observer"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -117,44 +118,64 @@ func Test_wrapHandler_error_creating_WorkflowVars(t *testing.T) {
 	assert.Equal(t, 500, w.Result().StatusCode)
 }
 
-func Test_wrapHandler_404_error_cannot_render_template(t *testing.T) {
-	w := httptest.NewRecorder()
-	r, _ := http.NewRequest(http.MethodGet, "test-url", nil)
-
-	mockClient := mockApiClient{
-		CurrentUserDetails: mockUserDetailsData,
-		TeamsForSelection:  mockTeamSelectionData,
+func Test_wrapHandler_status_error_handling(t *testing.T) {
+	tests := []struct {
+		error     error
+		wantCode  int
+		wantError string
+	}{
+		{error: StatusError(400), wantCode: 400, wantError: "400 Bad Request"},
+		{error: StatusError(401), wantCode: 401, wantError: "401 Unauthorized"},
+		{error: StatusError(403), wantCode: 403, wantError: "403 Forbidden"},
+		{error: StatusError(404), wantCode: 404, wantError: "404 Not Found"},
+		{error: StatusError(500), wantCode: 500, wantError: "500 Internal Server Error"},
+		{error: sirius.StatusError{Code: 400}, wantCode: 400, wantError: "  returned 400"},
+		{error: sirius.StatusError{Code: 401}, wantCode: 401, wantError: "  returned 401"},
+		{error: sirius.StatusError{Code: 403}, wantCode: 403, wantError: "  returned 403"},
+		{error: sirius.StatusError{Code: 404}, wantCode: 404, wantError: "  returned 404"},
+		{error: sirius.StatusError{Code: 500}, wantCode: 500, wantError: "  returned 500"},
 	}
+	for i, test := range tests {
+		t.Run("Scenario "+strconv.Itoa(i), func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodGet, "test-url", nil)
 
-	observedZapCore, observedLogs := observer.New(zap.InfoLevel)
-	logger := zap.New(observedZapCore).Sugar()
+			mockClient := mockApiClient{
+				CurrentUserDetails: mockUserDetailsData,
+				TeamsForSelection:  mockTeamSelectionData,
+			}
 
-	errorTemplate := &mockTemplate{error: errors.New("some template error")}
-	envVars := EnvironmentVars{}
-	nextHandlerFunc := wrapHandler(mockClient, logger, errorTemplate, envVars)
-	next := mockNext{Err: StatusError(404)}
-	httpHandler := nextHandlerFunc(next.GetHandler())
-	httpHandler.ServeHTTP(w, r)
+			observedZapCore, observedLogs := observer.New(zap.InfoLevel)
+			logger := zap.New(observedZapCore).Sugar()
 
-	logs := observedLogs.All()
+			errorTemplate := &mockTemplate{error: errors.New("some template error")}
+			envVars := EnvironmentVars{}
+			nextHandlerFunc := wrapHandler(mockClient, logger, errorTemplate, envVars)
+			next := mockNext{Err: test.error}
+			httpHandler := nextHandlerFunc(next.GetHandler())
+			httpHandler.ServeHTTP(w, r)
 
-	assert.Equal(t, 1, next.Called)
-	assert.Equal(t, w, next.w)
-	assert.Equal(t, r, next.r)
-	assert.Len(t, logs, 3)
-	assert.Equal(t, "Application Request", logs[0].Message)
-	assert.Len(t, logs[0].ContextMap(), 3)
-	assert.Equal(t, "GET", logs[0].ContextMap()["method"])
-	assert.Equal(t, "test-url", logs[0].ContextMap()["uri"])
-	assert.Equal(t, "Error handler", logs[1].Message)
-	assert.Equal(t, map[string]interface{}{"error": "404 Not Found"}, logs[1].ContextMap())
-	assert.Equal(t, "Failed to render error template", logs[2].Message)
-	assert.Equal(t, map[string]interface{}{"error": "some template error"}, logs[2].ContextMap())
-	assert.Equal(t, 1, errorTemplate.count)
-	assert.IsType(t, ErrorVars{}, errorTemplate.lastVars)
-	assert.Equal(t, 404, errorTemplate.lastVars.(ErrorVars).Code)
-	assert.Equal(t, "404 Not Found", errorTemplate.lastVars.(ErrorVars).Error)
-	assert.Equal(t, 404, w.Result().StatusCode)
+			logs := observedLogs.All()
+
+			assert.Equal(t, 1, next.Called)
+			assert.Equal(t, w, next.w)
+			assert.Equal(t, r, next.r)
+			assert.Len(t, logs, 3)
+			assert.Equal(t, "Application Request", logs[0].Message)
+			assert.Len(t, logs[0].ContextMap(), 3)
+			assert.Equal(t, "GET", logs[0].ContextMap()["method"])
+			assert.Equal(t, "test-url", logs[0].ContextMap()["uri"])
+			assert.Equal(t, "Error handler", logs[1].Message)
+			assert.Equal(t, map[string]interface{}{"error": test.wantError}, logs[1].ContextMap())
+			assert.Equal(t, "Failed to render error template", logs[2].Message)
+			assert.Equal(t, map[string]interface{}{"error": "some template error"}, logs[2].ContextMap())
+			assert.Equal(t, 1, errorTemplate.count)
+			assert.IsType(t, ErrorVars{}, errorTemplate.lastVars)
+			assert.Equal(t, test.wantCode, errorTemplate.lastVars.(ErrorVars).Code)
+			assert.Equal(t, test.wantError, errorTemplate.lastVars.(ErrorVars).Error)
+			assert.Equal(t, test.wantCode, w.Result().StatusCode)
+		})
+	}
 }
 
 func Test_wrapHandler_redirects_if_unauthorized(t *testing.T) {
