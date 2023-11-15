e3483ab358d975465043963f0d63c38d727806b7
nicholassully <nicholas.sully@digital.justice.gov.uk>
2023-11-15 10:40:30 +0000

    sw-6858 making the deputies tab visible #patch

--- internal/server/environment_vars_test.go
+++ internal/server/environment_vars_test.go
@@ -18,6 +18,5 @@ func TestNewEnvironmentVars(t *testing.T) {
 		DefaultWorkflowTeamID: 21,
 		DefaultPaTeamID:       "28",
 		DefaultProTeamID:      "31",
-		ShowDeputies:          false,
 	}, vars)
 }
