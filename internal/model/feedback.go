dd120f170b3d358fcd8f00cfac0f40ce23a3066f
Kate Smith <kate.smith@digital.justice.gov.uk>
2024-06-05 15:07:21 +0100

    sw-6661 finished form but need to test passing to api

---
+++ internal/model/feedback.go
@@ -0,0 +1,9 @@
+package model
+
+type FeedbackForm struct {
+	Id         int
+	Name       string
+	Email      string
+	CaseNumber string
+	Feedback   string
+}
--- internal/server/feedback-form.go
+++ internal/server/feedback-form.go
@@ -7,11 +7,38 @@ import (
 )
 
 type FeedbackFormClient interface {
-	GetCurrentUserDetails(sirius.Context) (model.Assignee, error)
+	SubmitFeedback(sirius.Context, model.FeedbackForm) error
 }
 
 func feedbackForm(client FeedbackFormClient, tmpl Template) Handler {
 	return func(app WorkflowVars, w http.ResponseWriter, r *http.Request) error {
+		if r.Method != http.MethodGet && r.Method != http.MethodPost {
+			return StatusError(http.StatusMethodNotAllowed)
+		}
+		ctx := getContext(r)
+
+		if r.Method == http.MethodPost {
+			err := r.ParseForm()
+			if err != nil {
+				return err
+			}
+
+			err = client.SubmitFeedback(ctx, model.FeedbackForm{
+				Id:         app.MyDetails.Id,
+				Name:       r.FormValue("name"),
+				Email:      r.FormValue("email"),
+				CaseNumber: r.FormValue("case-number"),
+				Feedback:   r.FormValue("more-detail"),
+			})
+
+			if err != nil {
+				return err
+			}
+
+			app.SuccessMessage = "Nice work"
+			//	do a redirect to the second page
+		}
+
 		return tmpl.Execute(w, app)
 	}
 }
