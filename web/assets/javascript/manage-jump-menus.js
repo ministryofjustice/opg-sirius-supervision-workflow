23deab037bc6ca4816e4be4592860e718b91c450
Sam Joseph <denialanderror@users.noreply.github.com>
2025-05-19 13:38:13 +0100

    Adds unsafe DOM read fixes, slow loris prevention, and safer file system parsing (#552)

--- web/assets/javascript/manage-jump-menus.js
+++ web/assets/javascript/manage-jump-menus.js
@@ -1,7 +1,17 @@
 export default class ManageJumpMenus {
   constructor(element) {
     element.addEventListener("change", function () {
-      window.location.href = this.options[this.selectedIndex].value;
+      const selectedValue = this.options[this.selectedIndex].value;
+      try {
+        const url = new URL(selectedValue, window.location.origin);
+        if (url.protocol === "http:" || url.protocol === "https:") {
+          window.location.href = url.href;
+        } else {
+          console.error("Invalid URL protocol:", url.protocol);
+        }
+      } catch (e) {
+        console.error("Invalid URL:", selectedValue);
+      }
     });
   }
 }
