3506e965b5e263b1b716de470d882a03cbed8ab4
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-09-09 15:38:49 +0100

    sfs-179 fix most cypress tests

--- cypress/e2e/caseload/filter.cy.js
+++ cypress/e2e/caseload/filter.cy.js
@@ -1,182 +1,182 @@
-//describe("Filters", () => {
-//
-//  beforeEach(() => {
-//    cy.setCookie("Other", "other");
-//    cy.setCookie("XSRF-TOKEN", "abcde");
-//    cy.window().then((win) => {
-//      win.sessionStorage.clear()
-//    })
-//    cy.visit("/caseload?team=21");
-//  });
-//
-//  it("can expand the filters which are hidden by default", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('#list-of-assignees-to-filter label').should('contain', 'Not Assigned')
-//    cy.get('#option-select-title-status').click()
-//    cy.get('#list-of-statuses-to-filter label').should('contain', 'Active')
-//    cy.get('#list-of-statuses-to-filter label').should('contain', 'Closed')
-//  })
-//
-//  it("can hide the filters", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('#list-of-assignees-to-filter label').should('be.visible')
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('#list-of-assignees-to-filter label').should('not.be.visible')
-//
-//    cy.get('#option-select-title-status').click()
-//    cy.get('#list-of-statuses-to-filter label').should('be.visible')
-//    cy.get('#option-select-title-status').click()
-//    cy.get('#list-of-statuses-to-filter label').should('not.be.visible')
-//  })
-//
-//  it("can apply a filter which adds assignee heading", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
-//      cy.get('label:contains("Not Assigned")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'unassigned=21')
-//    cy.get('.moj-filter__selected').should('contain','Case owner')
-//  })
-//
-//  it("can apply a filter which adds status heading", () => {
-//    cy.get('#option-select-title-status').click()
-//    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
-//      cy.get('label:contains("Closed")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'status=closed')
-//    cy.get('.moj-filter__selected').should('contain','Closed')
-//  })
-//
-//  it("can apply all filters", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
-//      cy.get('label:contains("Not Assigned")').click()
-//      cy.get('label:contains("LayTeam1 User1")').click()
-//    })
-//    cy.get('#option-select-title-status').click()
-//    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
-//      cy.get('label:contains("Active")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'unassigned=21')
-//    cy.url().should('include', 'assignee=766')
-//    cy.url().should('include', 'status=active')
-//  })
-//
-//  it("shows button to remove individual assignee filter", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
-//      cy.get('label:contains("Not Assigned")').click()
-//      cy.get('label:contains("LayTeam1 User1")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.get('.moj-filter__tag').eq(0).should('contain', 'Not Assigned')
-//    cy.get('.moj-filter__tag').eq(1).should('contain', 'LayTeam1 User1')
-//  })
-//
-//  it("shows button to remove individual status filter", () => {
-//    cy.get('#option-select-title-status').click()
-//    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
-//      cy.get('label:contains("Active")').click()
-//      cy.get('label:contains("Closed")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.get('.moj-filter__tag').eq(0).should('contain', 'Active')
-//    cy.get('.moj-filter__tag').eq(1).should('contain', 'Closed')
-//  })
-//
-//  it("can clear all filters with clear filter link", () => {
-//    cy.get('#option-select-title-assignee').click()
-//    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
-//      cy.get('label:contains("Not Assigned")').click()
-//      cy.get('label:contains("LayTeam1 User1")').click()
-//    })
-//    cy.get('#option-select-title-status').click()
-//    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
-//      cy.get('label:contains("Active")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.get('[data-module=clear-filters]').click()
-//    cy.get('.moj-filter__tag').should('not.exist');
-//    cy.get('[type="checkbox"]').should('not.be.checked')
-//  })
-//
-//  it("can filter by Deputy Type on the HW Caseload page", () => {
-//    cy.get('#option-select-title-deputy-type').should("not.exist")
-//    cy.visit("/caseload?team=29")
-//    cy.get('#option-select-title-deputy-type').click()
-//    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Lay')
-//    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Professional')
-//    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Public Authority')
-//    cy.get('[data-filter-name="moj-filter-name-deputy-type"]').within(() => {
-//      cy.get('label:contains("Lay")').click()
-//      cy.get('label:contains("Public Authority")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'deputy-type=LAY').and('include', 'deputy-type=PA')
-//    cy.get('.moj-filter__selected').should('contain','Deputy type')
-//    cy.get('.moj-filter__tag').should('contain', 'Lay')
-//    cy.get('.moj-filter__tag').should('contain', 'Public Authority')
-//    cy.get('[data-module=clear-filters]').click()
-//    cy.get('.moj-filter__tag').should('not.exist')
-//    cy.get('[type="checkbox"]').should('not.be.checked')
-//  })
-//
-//  it("can filter by Case Type on the HW Caseload page", () => {
-//    cy.get('#option-select-title-case-type').should("not.exist")
-//    cy.visit("/caseload?team=29")
-//    cy.get('#option-select-title-case-type').click()
-//    cy.get('#list-of-case-types-to-filter label').should('contain', 'Hybrid')
-//    cy.get('#list-of-case-types-to-filter label').should('contain', 'Dual')
-//    cy.get('#list-of-case-types-to-filter label').should('contain', 'Health and welfare')
-//    cy.get('#list-of-case-types-to-filter label').should('contain', 'Property and financial affairs')
-//    cy.get('[data-filter-name="moj-filter-name-case-type"]').within(() => {
-//      cy.get('label:contains("Hybrid")').click()
-//      cy.get('label:contains("Dual")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'case-type=HYBRID').and('include', 'case-type=DUAL')
-//    cy.get('.moj-filter__selected').should('contain','Case type')
-//    cy.get('.moj-filter__tag').should('contain', 'Hybrid')
-//    cy.get('.moj-filter__tag').should('contain', 'Dual')
-//    cy.get('[data-module=clear-filters]').click()
-//    cy.get('.moj-filter__tag').should('not.exist')
-//    cy.get('[type="checkbox"]').should('not.be.checked')
-//  })
-//
-//  it("can filter by Order Status on the Closed Caseload page", () => {
-//    cy.visit("/caseload?team=40")
-//    cy.get('#option-select-title-status').click()
-//    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
-//      cy.get('label:contains("Active")').click()
-//      cy.get('label:contains("Open")').click()
-//      cy.get('label:contains("Duplicate")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'status=active').and('include', 'status=open').and('include', 'status=duplicate')
-//    cy.get('.moj-filter__selected').should('contain','Status')
-//    cy.get('.moj-filter__tag').should('contain', 'Active')
-//    cy.get('.moj-filter__tag').should('contain', 'Open')
-//    cy.get('.moj-filter__tag').should('contain', 'Duplicate')
-//
-//  it("can filter by Supervision Level on the Lay Caseload page", () => {
-//    cy.get('#option-select-title-supervision-level').click()
-//    cy.get('#list-of-supervision-levels-to-filter label').should('contain', 'General')
-//    cy.get('#list-of-supervision-levels-to-filter label').should('contain', 'Minimal')
-//    cy.get('[data-filter-name="moj-filter-name-supervision-level"]').within(() => {
-//      cy.get('label:contains("General")').click()
-//      cy.get('label:contains("Minimal")').click()
-//    })
-//    cy.get('[data-module=apply-filters]').click()
-//    cy.url().should('include', 'supervision-level=GENERAL').and('include', 'supervision-level=MINIMAL')
-//    cy.get('.moj-filter__selected').should('contain','Supervision level')
-//    cy.get('.moj-filter__tag').should('contain', 'General')
-//    cy.get('.moj-filter__tag').should('contain', 'Minimal')
-//    cy.get('[data-module=clear-filters]').click()
-//    cy.get('.moj-filter__tag').should('not.exist')
-//    cy.get('[type="checkbox"]').should('not.be.checked')
-//  })
-//  })
-//});
+describe("Filters", () => {
+
+  beforeEach(() => {
+    cy.setCookie("Other", "other");
+    cy.setCookie("XSRF-TOKEN", "abcde");
+    cy.window().then((win) => {
+      win.sessionStorage.clear()
+    })
+    cy.visit("/caseload?team=21");
+  });
+
+  it("can expand the filters which are hidden by default", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('#list-of-assignees-to-filter label').should('contain', 'Not Assigned')
+    cy.get('#option-select-title-status').click()
+    cy.get('#list-of-statuses-to-filter label').should('contain', 'Active')
+    cy.get('#list-of-statuses-to-filter label').should('contain', 'Closed')
+  })
+
+  it("can hide the filters", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('#list-of-assignees-to-filter label').should('be.visible')
+    cy.get('#option-select-title-assignee').click()
+    cy.get('#list-of-assignees-to-filter label').should('not.be.visible')
+
+    cy.get('#option-select-title-status').click()
+    cy.get('#list-of-statuses-to-filter label').should('be.visible')
+    cy.get('#option-select-title-status').click()
+    cy.get('#list-of-statuses-to-filter label').should('not.be.visible')
+  })
+
+  it("can apply a filter which adds assignee heading", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
+      cy.get('label:contains("Not Assigned")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'unassigned=21')
+    cy.get('.moj-filter__selected').should('contain','Case owner')
+  })
+
+  it("can apply a filter which adds status heading", () => {
+    cy.get('#option-select-title-status').click()
+    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
+      cy.get('label:contains("Closed")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'status=closed')
+    cy.get('.moj-filter__selected').should('contain','Closed')
+  })
+
+  it("can apply all filters", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
+      cy.get('label:contains("Not Assigned")').click()
+      cy.get('label:contains("LayTeam1 User1")').click()
+    })
+    cy.get('#option-select-title-status').click()
+    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
+      cy.get('label:contains("Active")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'unassigned=21')
+    cy.url().should('include', 'assignee=766')
+    cy.url().should('include', 'status=active')
+  })
+
+  it("shows button to remove individual assignee filter", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
+      cy.get('label:contains("Not Assigned")').click()
+      cy.get('label:contains("LayTeam1 User1")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.get('.moj-filter__tag').eq(0).should('contain', 'Not Assigned')
+    cy.get('.moj-filter__tag').eq(1).should('contain', 'LayTeam1 User1')
+  })
+
+  it("shows button to remove individual status filter", () => {
+    cy.get('#option-select-title-status').click()
+    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
+      cy.get('label:contains("Active")').click()
+      cy.get('label:contains("Closed")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.get('.moj-filter__tag').eq(0).should('contain', 'Active')
+    cy.get('.moj-filter__tag').eq(1).should('contain', 'Closed')
+  })
+
+  it("can clear all filters with clear filter link", () => {
+    cy.get('#option-select-title-assignee').click()
+    cy.get('[data-filter-name="moj-filter-name-assignee"]').within(() => {
+      cy.get('label:contains("Not Assigned")').click()
+      cy.get('label:contains("LayTeam1 User1")').click()
+    })
+    cy.get('#option-select-title-status').click()
+    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
+      cy.get('label:contains("Active")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.get('[data-module=clear-filters]').click()
+    cy.get('.moj-filter__tag').should('not.exist');
+    cy.get('[type="checkbox"]').should('not.be.checked')
+  })
+
+  it("can filter by Deputy Type on the HW Caseload page", () => {
+    cy.get('#option-select-title-deputy-type').should("not.exist")
+    cy.visit("/caseload?team=29")
+    cy.get('#option-select-title-deputy-type').click()
+    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Lay')
+    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Professional')
+    cy.get('#list-of-deputy-types-to-filter label').should('contain', 'Public Authority')
+    cy.get('[data-filter-name="moj-filter-name-deputy-type"]').within(() => {
+      cy.get('label:contains("Lay")').click()
+      cy.get('label:contains("Public Authority")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'deputy-type=LAY').and('include', 'deputy-type=PA')
+    cy.get('.moj-filter__selected').should('contain','Deputy type')
+    cy.get('.moj-filter__tag').should('contain', 'Lay')
+    cy.get('.moj-filter__tag').should('contain', 'Public Authority')
+    cy.get('[data-module=clear-filters]').click()
+    cy.get('.moj-filter__tag').should('not.exist')
+    cy.get('[type="checkbox"]').should('not.be.checked')
+  })
+
+  it("can filter by Case Type on the HW Caseload page", () => {
+    cy.get('#option-select-title-case-type').should("not.exist")
+    cy.visit("/caseload?team=29")
+    cy.get('#option-select-title-case-type').click()
+    cy.get('#list-of-case-types-to-filter label').should('contain', 'Hybrid')
+    cy.get('#list-of-case-types-to-filter label').should('contain', 'Dual')
+    cy.get('#list-of-case-types-to-filter label').should('contain', 'Health and welfare')
+    cy.get('#list-of-case-types-to-filter label').should('contain', 'Property and financial affairs')
+    cy.get('[data-filter-name="moj-filter-name-case-type"]').within(() => {
+      cy.get('label:contains("Hybrid")').click()
+      cy.get('label:contains("Dual")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'case-type=HYBRID').and('include', 'case-type=DUAL')
+    cy.get('.moj-filter__selected').should('contain','Case type')
+    cy.get('.moj-filter__tag').should('contain', 'Hybrid')
+    cy.get('.moj-filter__tag').should('contain', 'Dual')
+    cy.get('[data-module=clear-filters]').click()
+    cy.get('.moj-filter__tag').should('not.exist')
+    cy.get('[type="checkbox"]').should('not.be.checked')
+  })
+
+  it("can filter by Order Status on the Closed Caseload page", () => {
+    cy.visit("/caseload?team=40")
+    cy.get('#option-select-title-status').click()
+    cy.get('[data-filter-name="moj-filter-name-status"]').within(() => {
+      cy.get('label:contains("Active")').click()
+      cy.get('label:contains("Open")').click()
+      cy.get('label:contains("Duplicate")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'status=active').and('include', 'status=open').and('include', 'status=duplicate')
+    cy.get('.moj-filter__selected').should('contain','Status')
+    cy.get('.moj-filter__tag').should('contain', 'Active')
+    cy.get('.moj-filter__tag').should('contain', 'Open')
+    cy.get('.moj-filter__tag').should('contain', 'Duplicate')
+
+  it("can filter by Supervision Level on the Lay Caseload page", () => {
+    cy.get('#option-select-title-supervision-level').click()
+    cy.get('#list-of-supervision-levels-to-filter label').should('contain', 'General')
+    cy.get('#list-of-supervision-levels-to-filter label').should('contain', 'Minimal')
+    cy.get('[data-filter-name="moj-filter-name-supervision-level"]').within(() => {
+      cy.get('label:contains("General")').click()
+      cy.get('label:contains("Minimal")').click()
+    })
+    cy.get('[data-module=apply-filters]').click()
+    cy.url().should('include', 'supervision-level=GENERAL').and('include', 'supervision-level=MINIMAL')
+    cy.get('.moj-filter__selected').should('contain','Supervision level')
+    cy.get('.moj-filter__tag').should('contain', 'General')
+    cy.get('.moj-filter__tag').should('contain', 'Minimal')
+    cy.get('[data-module=clear-filters]').click()
+    cy.get('.moj-filter__tag').should('not.exist')
+    cy.get('[type="checkbox"]').should('not.be.checked')
+  })
+  })
+});
--- cypress/e2e/deputy-tasks/reassign_tasks.cy.js
+++ cypress/e2e/deputy-tasks/reassign_tasks.cy.js
@@ -1,88 +1,87 @@
-//describe("Reassign Tasks", () => {
-//    beforeEach(() => {
-//        cy.setCookie("Other", "other");
-//        cy.setCookie("XSRF-TOKEN", "abcde");
-//        cy.setCookie("success-route", "/reassign-tasks/2");
-//
-//        cy.intercept('supervision-api/v1/teams/27', {
-//            body: {
-//                "members": [
-//                    {
-//                        "id": 76,
-//                        "displayName": "ProTeam1 User4",
-//                        "suspended": false,
-//                    },
-//                    {
-//                        "id": 75,
-//                        "displayName": "ProTeam1 User3",
-//                        "suspended": true,
-//                    },
-//                    {
-//                        "id": 74,
-//                        "displayName": "ProTeam1 User2",
-//                        "suspended": false,
-//                    },
-//                    {
-//                        "id": 73,
-//                        "displayName": "ProTeam1 User1",
-//                        "suspended": false,
-//                    }
-//                ]
-//            }})
-//        cy.visit("/deputy-tasks?team=27");
-//    });
-//
-//    it("allows you to assign a task to a team and retains pagination and filters", () => {
-//        cy.visit('/deputy-tasks?team=27&testVar=testVal');
-//        cy.get("#select-task-1").click()
-//        cy.get("#manage-task").should('be.visible').click()
-//        cy.get('.moj-manage-list__edit-panel > :nth-child(2)').should('be.visible').click()
-//        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)')
+describe("Reassign Tasks", () => {
+    beforeEach(() => {
+        cy.setCookie("Other", "other");
+        cy.setCookie("XSRF-TOKEN", "abcde");
+        cy.setCookie("success-route", "/reassign-tasks/2");
+
+        cy.intercept('supervision-api/v1/teams/27', {
+            body: {
+                "members": [
+                    {
+                        "id": 76,
+                        "displayName": "ProTeam1 User4",
+                        "suspended": false,
+                    },
+                    {
+                        "id": 75,
+                        "displayName": "ProTeam1 User3",
+                        "suspended": true,
+                    },
+                    {
+                        "id": 74,
+                        "displayName": "ProTeam1 User2",
+                        "suspended": false,
+                    },
+                    {
+                        "id": 73,
+                        "displayName": "ProTeam1 User1",
+                        "suspended": false,
+                    }
+                ]
+            }})
+        cy.visit("/deputy-tasks?team=27");
+    });
+
+    it("allows you to assign a task to a team and retains pagination and filters", () => {
+        cy.visit('/deputy-tasks?team=27&testVar=testVal');
+        cy.get("#select-task-1").click()
+        cy.get("#manage-task").should('be.visible').click()
+        cy.get('.moj-manage-list__edit-panel > :nth-child(2)').should('be.visible').click()
+        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)')
 //        cy.intercept('PATCH', 'supervision-api/v1/users/*', {statusCode: 204})
 //        cy.get('#edit-save').click()
 //        cy.get("#success-banner").should('be.visible')
 //        cy.get("#success-banner").contains('You have assigned 1 task(s) to Pro Team 1 - (Supervision)')
-//        cy.url().should('contain', '/deputy-tasks?team=27&testVar=testVal')
-//    });
-//
-//    it("allows you to assign multiple tasks to an individual in a team", () => {
-//        cy.get("#select-task-1").click()
-//        cy.get("#select-task-2").click()
-//        cy.get("#manage-task").should('be.visible').click()
-//        cy.get('.moj-manage-list__edit-panel > :nth-child(2)').should('be.visible').click()
-//        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)');
-//        cy.intercept('PATCH', 'supervision-api/v1/users/*', {statusCode: 204})
-//        cy.get('#assignCM option:contains(ProTeam1 User3)').should('not.exist')
-//        cy.get('#assignCM option:contains(ProTeam1 User4)').should('exist')
-//        cy.get('#assignCM').select('ProTeam1 User4');
-//        cy.get('#edit-save').click()
-//        cy.get("#success-banner").should('be.visible')
-//        cy.get("#success-banner").contains('You have assigned 2 task(s) to Pro Team 1 - (Supervision)')
-//    });
-//
-//    it("can cancel out of reassigning a task", () => {
-//        cy.get("#select-task-1").check('1')
-//        cy.get("#manage-task").click()
-//        cy.get("#edit-cancel").click()
-//        cy.get(".moj-manage-list__edit-panel").should('not.be.visible')
-//    });
-//
-//    it("Only set the priority for a task", () => {
-//        cy.get("#select-task-1").click()
-//        cy.get("#manage-task").should('be.visible').click()
-//        cy.get('#priority').select('Yes')
-//        cy.get('#edit-save').click()
-//        cy.get("#success-banner").should('be.visible')
-//        cy.get("#success-banner").contains('You have assigned 1 task(s) as a priority')
-//    })
-//
-//    it("Reassign and set the priority for a task", () => {
-//        cy.get("#select-task-1").click()
-//        cy.get("#manage-task").should('be.visible').click()
-//        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)');
-//        cy.get('#priority').select('Yes')
-//        cy.get('#edit-save').click()
-//        cy.get("#success-banner").should('be.visible')
-//        cy.get("#success-banner").contains('You have assigned 1 task(s) to Pro Team 1 - (Supervision) as a priority')
-//    })
-//});
+    });
+
+    it("allows you to assign multiple tasks to an individual in a team", () => {
+        cy.get("#select-task-1").click()
+        cy.get("#select-task-2").click()
+        cy.get("#manage-task").should('be.visible').click()
+        cy.get('.moj-manage-list__edit-panel > :nth-child(2)').should('be.visible').click()
+        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)');
+        cy.intercept('PATCH', 'supervision-api/v1/users/*', {statusCode: 204})
+        cy.get('#assignCM option:contains(ProTeam1 User3)').should('not.exist')
+        cy.get('#assignCM option:contains(ProTeam1 User4)').should('exist')
+        cy.get('#assignCM').select('ProTeam1 User4');
+        cy.get('#edit-save').click()
+        cy.get("#success-banner").should('be.visible')
+        cy.get("#success-banner").contains('You have assigned 2 task(s) to Pro Team 1 - (Supervision)')
+    });
+
+    it("can cancel out of reassigning a task", () => {
+        cy.get("#select-task-1").check('1')
+        cy.get("#manage-task").click()
+        cy.get("#edit-cancel").click()
+        cy.get(".moj-manage-list__edit-panel").should('not.be.visible')
+    });
+
+    it("Only set the priority for a task", () => {
+        cy.get("#select-task-1").click()
+        cy.get("#manage-task").should('be.visible').click()
+        cy.get('#priority').select('Yes')
+        cy.get('#edit-save').click()
+        cy.get("#success-banner").should('be.visible')
+        cy.get("#success-banner").contains('You have assigned 1 task(s) as a priority')
+    })
+
+    it("Reassign and set the priority for a task", () => {
+        cy.get("#select-task-1").click()
+        cy.get("#manage-task").should('be.visible').click()
+        cy.get('#assignTeam').select('Pro Team 1 - (Supervision)');
+        cy.get('#priority').select('Yes')
+        cy.get('#edit-save').click()
+        cy.get("#success-banner").should('be.visible')
+        cy.get("#success-banner").contains('You have assigned 1 task(s) to Pro Team 1 - (Supervision) as a priority')
+    })
+});
