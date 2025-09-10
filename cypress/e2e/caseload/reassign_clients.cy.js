6ef5602596d6002bdcb3133a929ba44dfbbe86eb
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-09-10 08:57:33 +0100

    sfs-179 fix cypress tests

--- cypress/e2e/caseload/reassign_clients.cy.js
+++ cypress/e2e/caseload/reassign_clients.cy.js
@@ -4,24 +4,9 @@ describe("Reassign clients", () => {
     "/caseload?team=28"
   ]
 
-  before(() => {
+  beforeEach(() => {
     cy.setCookie("Other", "other");
     cy.setCookie("XSRF-TOKEN", "abcde");
-  });
-
-  it("can cancel out of reassigning a client", () => {
-    pages.forEach((page) => {
-      cy.visit(page)
-      cy.get('#select-client-63').click();
-      cy.get("#manage-client").should('be.visible').click();
-      cy.get("#edit-cancel").click();
-      cy.get(".moj-manage-list__edit-panel").should('not.be.visible');
-    })
-  });
-
-  it("allows you to reassign a client", () => {
-    pages.forEach((page) => {
-      cy.visit(page)
       cy.intercept('supervision-api/v1/teams/21', {
         body: {
           "members": [
@@ -48,6 +33,36 @@ describe("Reassign clients", () => {
           ]
         }
       })
+  });
+
+  it("can cancel out of reassigning a client", () => {
+    pages.forEach((page) => {
+      cy.visit(page)
+      cy.get('#select-client-63').click();
+      cy.get("#manage-client").should('be.visible').click();
+      cy.get("#edit-cancel").click();
+      cy.get(".moj-manage-list__edit-panel").should('not.be.visible');
+    })
+  });
+
+it("allows you to reassign a client for normal lay team", () => {
+    cy.visit("/caseload?team=21")
+    cy.setCookie("success-route", "/reassign-clients/1");
+    cy.get('#select-client-63').click();
+    cy.get("#manage-client").should('be.visible').click();
+    cy.get('.moj-manage-list__edit-panel > :nth-child(2)').should('be.visible').click()
+    cy.get('#assignTeam').select('Lay Team 1 - (Supervision)');
+    cy.intercept('PATCH', 'supervision-api/v1/users/*', {statusCode: 204})
+    cy.get('#assignCM option:contains(LayTeam1 User3)').should('not.exist')
+    cy.get('#assignCM option:contains(LayTeam1 User1)').should('exist')
+    cy.get('#assignCM').select('LayTeam1 User1');
+    cy.get('#edit-save').click()
+    cy.get("#success-banner").should('be.visible')
+    cy.get("#success-banner").contains('You have reassigned ')
+  });
+
+  it("allows you to reassign a client for lay new deputy team", () => {
+      cy.visit("/caseload?team=28")
       cy.setCookie("success-route", "/reassign-clients/1");
       cy.get('#select-client-63').click();
       cy.get("#manage-client").should('be.visible').click();
@@ -60,6 +75,5 @@ describe("Reassign clients", () => {
       cy.get('#edit-save').click()
       cy.get("#success-banner").should('be.visible')
       cy.get("#success-banner").contains('You have reassigned ')
-    })
   });
 })
