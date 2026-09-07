22f17035c7c88ebbfc1db8c6d9205a02957bd12d
Sam Joseph <denialanderror@googlemail.com>
2026-09-07 17:48:53 +0100

    sfs-376: Refactors reassign functions to not include data in the requests that shouldn't be serialised

--- internal/sirius/reassign_clients.go
+++ internal/sirius/reassign_clients.go
@@ -12,6 +12,10 @@ import (
 type ReassignClientsParams struct {
 	AssignTeam string
 	AssignCM   string
+	ClientIds  []string
+}
+
+type ReassignClientsRequest struct {
 	AssigneeId int      `json:"assigneeId"`
 	ClientIds  []string `json:"clientIds"`
 	IsWorkflow bool     `json:"isWorkflow"`
@@ -31,13 +35,16 @@ func (c *ApiClient) ReassignClients(ctx Context, params ReassignClientsParams) (
 		assignee = params.AssignCM
 	}
 
-	params.AssigneeId, err = strconv.Atoi(assignee)
+	id, err := strconv.Atoi(assignee)
 	if err != nil {
 		return "", err
 	}
 
-	params.IsWorkflow = true
-	err = json.NewEncoder(&body).Encode(params)
+	err = json.NewEncoder(&body).Encode(ReassignClientsRequest{
+		AssigneeId: id,
+		ClientIds:  params.ClientIds,
+		IsWorkflow: true,
+	})
 
 	if err != nil {
 		return "", err
