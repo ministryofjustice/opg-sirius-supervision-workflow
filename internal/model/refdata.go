91f1dff4fb9375f94e04e515b9653299c9549865
Sam Joseph <denialanderror@googlemail.com>
2026-09-09 11:12:11 +0100

    sfs-376: First pass at making all API requests deserialise to DTOs to better match the JSON structure and avoid circular dependencies

--- internal/model/refdata.go
+++ internal/model/refdata.go
@@ -1,5 +1,7 @@
 package model
 
+import "slices"
+
 type RefData struct {
 	Handle string `json:"handle"`
 	Label  string `json:"label"`
@@ -10,10 +12,5 @@ func (r RefData) Is(handle string) bool {
 }
 
 func (r RefData) IsIn(handles []string) bool {
-	for _, handle := range handles {
-		if r.Is(handle) {
-			return true
-		}
-	}
-	return false
+	return slices.ContainsFunc(handles, r.Is)
 }
