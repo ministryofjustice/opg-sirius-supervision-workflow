1e7d7d2d5b11e19f59bcdd39414efe4bb67543cb
Sam Joseph <denialanderror@googlemail.com>
2026-09-11 09:14:36 +0100

    sfs-376: Skipping pacts that either work or can't be implemented to provide a cleaner contract for debugging failures

--- internal/sirius/reassign_clients_test.go
+++ internal/sirius/reassign_clients_test.go
@@ -127,6 +127,7 @@ func TestReassignClientsReturnsInternalServerError(t *testing.T) {
 }
 
 func TestReassignClients_contract(t *testing.T) {
+	t.Skip("Working, so skip to allow for cleaner fail logs")
 	pact, err := consumer.NewV4Pact(consumer.MockHTTPProviderConfig{
 		Consumer: "sirius-supervision-workflow",
 		Provider: "sirius",
